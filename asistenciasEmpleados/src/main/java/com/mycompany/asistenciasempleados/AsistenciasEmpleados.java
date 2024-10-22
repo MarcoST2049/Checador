bd11e3071096571ad971fe2622f1cc4d89d867b7
MarcoST2049 <quirozsotomarcoantonio81@gmail.com>
2024-10-21 20:59:55 -0700

    commit 1

---
+++ asistenciasEmpleados/src/main/java/com/mycompany/asistenciasempleados/AsistenciasEmpleados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asistenciasempleados;
+
+/**
+ *
+ * @author adria
+ */
+public class AsistenciasEmpleados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
